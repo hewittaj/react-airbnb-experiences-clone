bea1c2a0d094cb19b27d5f0404543edf93c00575
Alex Hewitt <alexhewitt.j@gmail.com>
2023-11-16 14:34:23 -0600

    add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import heroGrid from '../assets/photo-grid.png'
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <img src={heroGrid} id='hero-img'/>
+            <div className='hero-text'>
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activites led by one-of-a-kind hosts
+                    all without leaving home.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
